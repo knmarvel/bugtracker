b4ef7050a377b772a8e352013279007408d55a45
Karen Marvel <knmarvel@gmail.com>
2020-05-21 10:58:58 -0400

    set up with custom users

---
+++ bugs/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+
+
+class MyUser(AbstractUser):
+    display_name = models.CharField(
+        max_length=150,
+        null=True,
+        blank=True
+    )
+    REQUIRED_FIELDS = ['display_name']
